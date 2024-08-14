f237b74d3ef59035082adc8c96cdc35c21a4558e
Charith Lakshan <139710239+charith3903@users.noreply.github.com>
2024-08-14 20:09:48 +0530

    first commite

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,22 @@
+<head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="PixcelsThemes">
+    <!-- Favicon Icon -->
+    <link rel="icon" href="assets/img/favicon.png">
+    <!-- Site Title -->
+    <title>Bizmax - Corporate And Consulting Business Template</title>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins/swiper.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins/odometer.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins/jquery-ui.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/default.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  </head>
---
+++ resources/views/components/script.blade.php
@@ -0,0 +1,10 @@
+    <script src="{{ asset('assets/js/plugins/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/isotope.pkg.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/jquery.slick.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/odometer.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/ripples.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/swiper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/YTPlayer.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
